40c4ee401aaa022769c6bc8fad08e392bf6b34c9
JiGuoliang <sigmadfzb@gmail.com>
2023-07-01 22:50:01 +0800

    Update domUtils.ts

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -4,15 +4,19 @@ function useWindowDimensions() {
   const width = ref<number>(window.innerWidth);
   const height = ref<number>(window.innerHeight);
 
+  const winRatio = ref<number>(window.innerWidth / window.innerHeight);
+  const isMobile = ref<boolean>(window.innerWidth / window.innerHeight ? false : true)
+
   const listener = () => {
     width.value = window.innerWidth;
     height.value = window.innerHeight;
+    isMobile.value = window.innerWidth / window.innerHeight ? false : true
   };
 
   window.addEventListener("resize", listener, false);
   onBeforeUnmount(() => {
     window.removeEventListener("resize", listener, false);
   });
-  return { width, height };
+  return { width, height, isMobile };
 }
 export { useWindowDimensions };
